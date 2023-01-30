88b4b9bc8a3d599b277120e4cf1404933ebd5b69
Hrutvik Yadav <hrutvikyadav@gmail.com>
2023-01-31 00:21:13 +0530

    feat: added index action and links to move around

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
 
     def new
-
         @user = User.new
     end
 
@@ -10,7 +12,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            redirect_to new_user_path
+            redirect_to @user
         else
             render :new, status: :unprocessable_entity
         end
