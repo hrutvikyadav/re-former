24228791e4505fbb0e0fafb7be9efe69b8b3ea30
hrutvikyadav <hrutvikyadav@gmail.com>
2023-01-30 17:50:22 +0530

    feat: ruby forms added tut

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+    def new
+
+        @user = User.new
+    end
+
+    def create
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @user = User.find_by(id: params[:id])
+    end
+
+    def update
+        @user = User.find_by(id: params[:id])
+        if @user.update(user_params)
+            redirect_to user_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+end
